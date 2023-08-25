75ec366b2c657ef17dfeb7273d3aaed746d25309
imrismaa <rismasaputri@mail.ugm.ac.id>
2023-08-26 00:34:20 +0700

    add main activity

--- app/src/main/java/com/example/loginpage_pppb/MainActivity.kt
+++ app/src/main/java/com/example/loginpage_pppb/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.loginpage_pppb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.loginpage_pppb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var correctUsername: String
+    private lateinit var correctPassword: String
     override fun onCreate(savedInstanceState: Bundle?) {
-        val email = "risma@gmail.com"
-        val pass = "hakunamatat4"
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        correctUsername = "imrismaa"
+        correctPassword = "hakunamatat4"
+
+        binding.loginbtn.setOnClickListener {
+            val usernameInput = binding.username.text.toString()
+            val passwordInput = binding.password.text.toString()
+
+            if (usernameInput == correctUsername && passwordInput == correctPassword) {
+                Toast.makeText(this@MainActivity, "Login Sukses", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this@MainActivity, "username atau assword salah", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
